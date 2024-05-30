59f377162317f7b3769a12eb320a797264da00f1
Jens Jäger <jaegerj204@googlemail.com>
2024-05-30 12:17:10 +0200

    replace default with reveal button and use tailwind css

--- src/app/components/reveal-button.tsx
+++ src/app/components/reveal-button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useCallback, useState } from "react";
 
 export interface RevealButtonProps {
@@ -13,5 +15,12 @@ export const RevealButton = ({ secret }: RevealButtonProps) => {
   if (revealed) {
     return <div>{secret}</div>;
   }
-  return <button onClick={onClick}>Reveal</button>;
+  return (
+    <button
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 focus:outline-none dark:focus:ring-blue-800"
+      onClick={onClick}
+    >
+      Reveal
+    </button>
+  );
 };
